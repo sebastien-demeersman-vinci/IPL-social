87fd7b5d2af254c19ec8cbf935685f975baf7768
sebastien-demeersman-vinci <sebastien.demeersman@student.vinci.be>
2026-01-16 16:31:22 +0100

    refactor: create only one instance of EmailCheckere for tests

--- emailChecker.test.ts
+++ emailChecker.test.ts
@@ -1,25 +1,21 @@
 import { EmailChecker } from "./emailChecker";
 
 describe("Email Checker", () => {
-  it("should return false when given an email without @", () => {
     const emailChecker = new EmailChecker();
 
+  it("should return false when given an email without @", () => {
     const actual = emailChecker.check("vinci.be");
 
     expect(actual).toBe(false);
   });
 
   it("should return false when given an email with @ in first char", () => {
-    const emailChecker = new EmailChecker();
-
     const actual = emailChecker.check("@vinci.be");
 
     expect(actual).toBe(false);
   });
 
   it("should return false when given an email with @ in last char", () => {
-    const emailChecker = new EmailChecker();
-
     const actual = emailChecker.check("vinci.be@");
 
     expect(actual).toBe(false);
